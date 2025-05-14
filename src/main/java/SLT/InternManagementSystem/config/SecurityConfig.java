da3d2e23c2fc297470ea36ebfba94731af70cfc4
Sandani Gunawardhana <siagunawardhana@gmail.com>
2025-05-14 10:28:05 +0530

    CORS configuration fixed

--- src/main/java/SLT/InternManagementSystem/config/SecurityConfig.java
+++ src/main/java/SLT/InternManagementSystem/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import SLT.InternManagementSystem.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                       req -> req.requestMatchers("/login/**", "/signup/manual", "/verify", "/signup/google").permitAll()
