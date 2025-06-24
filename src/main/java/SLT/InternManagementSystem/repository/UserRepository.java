9a4c968a8601b10536c958c2c4bff44f15fc65cc
sakith <sakiththewmika71@gmail.com>
2025-06-24 08:38:05 +0530

    Update UserRepository.java

--- src/main/java/SLT/InternManagementSystem/repository/UserRepository.java
+++ src/main/java/SLT/InternManagementSystem/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     Optional<User> findById(int id);
     boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
+
 }
